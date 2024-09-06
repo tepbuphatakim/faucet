64d9fd5bc1efc60a8c1aa9082e61ab59912d9840
tepbuphata.kim <kimtepbuphata009@gmail.com>
2024-09-06 15:06:03 +0700

    feat: constructor payable contract

---
+++ contracts/Payable.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Payable {
+  address public owner;
+
+  constructor() payable {
+    owner = msg.sender;
+  }
+
+  function getContractBalance() public view returns (uint256) {
+    return address(this).balance;
+  }
+
+  function withdraw(uint256 amount) public {
+    require(msg.sender == owner, "Only the owner can send Ether");
+    require(address(this).balance >= amount, "Insufficient balance");
+
+    (bool sent, ) = owner.call{value: amount}("");
+    require(sent, "Failed to send Ether");
+  }
+
+  receive() external payable {}
+}
